52644f31cca8bd2e7d07adeeb2c9530bf28822ab
Arihant Gat <arihantgat@gmail.com>
2025-01-08 11:56:09 +0530

    new branch

--- src/cucumber/Options/TestRunner.java
+++ src/cucumber/Options/TestRunner.java
@@ -3,7 +3,7 @@ package cucumber.Options;
 import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
-//Git
+//Git demo
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/features", plugin = "json:target/jsonReports/cucumber-report.json", glue = {"stepDefinations" }, tags= "@AddPlace")
 public class TestRunner {
